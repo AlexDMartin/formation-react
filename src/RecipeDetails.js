007d0d6670be8eae9db8780090e78b22b7bb1ef9
Alex Martin <amartin@excilys.com>
2019-04-11 14:59:56 +0200

    [EDIT] Adding Edit Feature

--- src/RecipeDetails.js
+++ src/RecipeDetails.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
-import { Card, CardTitle, CardBody, CardSubtitle, CardText, CardImg, Button, Row, Col, ButtonGroup, Container } from 'reactstrap';
+import { Card, CardTitle, CardBody, CardText, CardImg, Button, Row, Col, ButtonGroup, Container, Input } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes, faPen, faEye } from '@fortawesome/free-solid-svg-icons'
 
 class RecipeDetails extends Component {
     state = {
-        recipe: this.props.recipe
+        recipe: this.props.recipe,
+        editMode: false
+    }
+
+    toggleEditMode = (event) => this.setState({editMode: !this.state.editMode})
+
+    onChangePicture = (event) => {
+        console.debug('Picture changed');
+        this.setState({
+            recipe: {
+                ...this.state.recipe, picture: event.target.value
+            }
+        })
+    }
+
+    onChangeName = (event) => {
+        console.debug('Name changed');
+        this.setState({
+            recipe: {
+                ...this.state.recipe, name: event.target.value
+            }
+        })
+    }
+
+    onChangeDescription = (event) => {
+        console.debug('Description changed');
+        this.setState({
+            recipe: {
+                ...this.state.recipe, description: event.target.value
+            }
+        })
     }
 
     render() {
@@ -18,13 +48,22 @@ class RecipeDetails extends Component {
                 <Col md={{size: 4, offset:4}}>
                 <div>
                   <Card>
-                    <CardImg top width="100%" src={recipe.picture} alt="Card image cap" />
+                      {this.state.editMode && <Input value={recipe.picture} onChange={this.onChangePicture}/>}
+                    <CardImg top width="100%" src={recipe.picture} alt="Recipe Image" />
                     <CardBody>
-                      <CardTitle>{recipe.name}</CardTitle>
-                      <CardSubtitle>Card subtitle</CardSubtitle>
-                      <CardText>{recipe.description}</CardText>
+
+                    {this.state.editMode
+                        ? <Input onChange={this.onChangeName} value={recipe.name}/>
+                        : <CardTitle>{recipe.name}</CardTitle>
+                    }
+
+                    {this.state.editMode
+                        ? <textarea onChange={this.onChangeDescription} value={recipe.description}/>
+                        : <CardText>{recipe.description}</CardText>
+                    } 
+                     
                       <ButtonGroup>
-                        <Button color="primary"><FontAwesomeIcon icon={faPen}/></Button>
+                        <Button color="primary" onClick={this.toggleEditMode}><FontAwesomeIcon icon={faPen}/></Button>
                         <Button color="primary"><FontAwesomeIcon icon={faEye}/></Button>
                         <Button color="danger" onClick={this.props.onDelete(recipe)}><FontAwesomeIcon icon={faTimes}/></Button>
                       </ButtonGroup>
